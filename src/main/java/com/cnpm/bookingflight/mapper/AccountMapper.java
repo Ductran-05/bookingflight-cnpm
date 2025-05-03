805665c32f8dda19318e5952dd157610c3c78b13
DucTran-05 <duc30092005@gmail.com>
2025-05-03 16:18:03 +0700

    chore(account): fix update API

--- src/main/java/com/cnpm/bookingflight/mapper/AccountMapper.java
+++ src/main/java/com/cnpm/bookingflight/mapper/AccountMapper.java
@@ -1,15 +1,33 @@
 package com.cnpm.bookingflight.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
 import com.cnpm.bookingflight.domain.Account;
 import com.cnpm.bookingflight.dto.request.AccountRequest;
+import com.cnpm.bookingflight.exception.AppException;
+import com.cnpm.bookingflight.exception.ErrorCode;
+import com.cnpm.bookingflight.repository.RoleRepository;
 
-@Mapper(componentModel = "spring")
-public interface AccountMapper {
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
-    @Mapping(target = "id", ignore = true)
-    public Account toAccount(AccountRequest request);
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Component
+public class AccountMapper {
+    final RoleRepository roleRepository;
 
+    public Account toAccount(AccountRequest request) {
+        return Account.builder()
+                .email(request.getEmail())
+                .fullName(request.getFullName())
+                .phone(request.getPhone())
+                .avatar(request.getAvatar())
+                .username(request.getUsername())
+                .password(request.getPassword())
+                .role(roleRepository.findById(request.getRoleId())
+                        .orElseThrow(() -> new AppException(ErrorCode.INVALID)))
+                .build();
+    }
 }
