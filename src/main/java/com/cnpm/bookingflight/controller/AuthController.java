9ade10c38d7b668545954cdd8da137c7a4bbce6d
DucTran-05 <duc30092005@gmail.com>
2025-06-07 08:52:18 +0700

    fix: rename endpoint

--- src/main/java/com/cnpm/bookingflight/controller/AuthController.java
+++ src/main/java/com/cnpm/bookingflight/controller/AuthController.java
@@ -101,7 +101,7 @@ public class AuthController {
         }
 
         // lay nguoi dung da dang nhap
-        @GetMapping("/my-account")
+        @GetMapping("/user")
         public ResponseEntity<APIResponse<AccountResponse>> getUserLogin() {
                 String username = SecurityUtil.getCurrentUserLogin().isPresent()
                                 ? SecurityUtil.getCurrentUserLogin().get()
